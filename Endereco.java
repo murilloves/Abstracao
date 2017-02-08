e536512af0ed7ae7f3b8bb7f4bfdfab9b1e77a25
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-02-08 13:41:02 -0200

    Create Endereco.java

---
+++ Endereco.java
@@ -0,0 +1,18 @@
+package abstracao;
+
+public class Endereco {
+    private String rua;
+    private int numero;
+    private int quadra;
+    private int lote;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    
+    public Endereco(String rua, int numero){
+        this.rua = rua;
+        this.numero = numero;
+    }
+    
+}
